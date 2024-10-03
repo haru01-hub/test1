bf5dea3f4bb4560cf070ca0e5558209e0b715b97
이서연 <iseoyeon@iseoyeon-ui-MacBookPro.local>
2024-10-03 22:53:49 +0900

    2nd

--- app/posts/new/page.tsx
+++ app/posts/new/page.tsx
@@ -4,7 +4,11 @@ export default function NewPostPage() {
   return (
     <div>
       NewPostPage
-      <p></p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo natus,
+        necessitatibus, modi fuga soluta culpa saepe amet omnis, ratione quos
+        labore at rem? Molestiae error beatae recusandae aliquid provident quae!
+      </p>
     </div>
   )
 }
